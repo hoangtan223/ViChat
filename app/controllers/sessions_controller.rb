b793540e2c2a502d59e9d34a4f6a749c73baf9f7
Tanbui <hoangtan223@gmail.com>
2016-07-14 23:35:47 +0700

    Finish implement Login, Logout and Register function

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	if @user = User.find_by_email(params[:email])
+  		if @user.authenticate params[:password]
+  			session[:user_id] = @user.id
+  			flash[:success] = "Logged in!"
+  			redirect_to root_path
+  		else
+  			redirect_to 'new', flash[:error] = "Wrong password!"
+  		end
+  	else
+  		redirect_to 'new', flash[:error] = "Cannot find user!"
+  	end
+  end
+
+  def destroy
+  	session[:user_id] = nil
+  	flash[:success] = "Logged out!"
+  	redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+
   get 'users/new'
+
+  get '/login' => 'sessions#new'
+
+  delete 'logout' => 'sessions#destroy'
 
   root 'home#index'
 
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
