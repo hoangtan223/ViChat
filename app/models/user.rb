a2e8d9fed5be52dfaeb377ba04eb17f820ccb5ed
Tanbui <hoangtan223@gmail.com>
2016-07-15 00:16:21 +0700

    Generate message model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
 
 	validates :name, presence: true
 	validates :email, uniqueness: true
+
+	has_many :sent_messages, foreign_key: 'sender_id', class_name: 'Message'
+	has_many :received_messages, foreign_key: 'recepient_id', class_name: 'Message'
 end
